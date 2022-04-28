fd7fae57db616f7030fa38b3cbd1ba59b77b58f3
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-04-28 10:57:39 -0300

    setup heroku

--- prisma/seeds.ts
+++ prisma/seeds.ts
@@ -3,15 +3,15 @@ import prisma from "../src/database.js";
 //need update down below
 async function main() {
     console.log('seeds.ts needs update')
-	await prisma.memes.upsert({
-    where: { title: "meu meme" },
-    update: {},
-    create: {
-      title: "meu meme",
-      description: "meu memezin",
-      url: "http://google.com",
-    },
-  });
+// 	await prisma.memes.upsert({
+//     where: { title: "meu meme" },
+//     update: {},
+//     create: {
+//       title: "meu meme",
+//       description: "meu memezin",
+//       url: "http://google.com",
+//     },
+//   });
 }
 
 main()
